fecee12b94932ae48e413af1a4035f515f1c9dfa
aliev008 <hfe.as@yandex.ru>
2022-07-19 12:34:35 +0300

    refactor: deleted redundant states

--- src/index.js
+++ src/index.js
@@ -1,57 +1,71 @@
-import React, { useEffect, useState } from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import randomColor from "./utils/randomColor";
 
 const App = () => {
-  console.log('App rendered');
-  const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+  console.log("App rendered");
   const [data, setData] = useState(null);
-  const [currentQuote, setCurrentQuote] = useState('');
-  const [author, setAuthor] = useState('');
-  const [backgroundColor, setBackgroundColor] = useState(randomColor);
-  const [color, setColor] = useState(randomColor)
-  const [opacity, setOpacity] = useState(0);
+  const [color, setColor] = useState(randomColor);
   const [buttonClicked, setButtonClicked] = useState(false);
+  let opacity = 1;
 
   useEffect(() => {
-    console.log('UseEffect rendered');
-    fetch('https://api.quotable.io/random')
-    .then(response => response.json())
-    .then(data => setData(data))}, [buttonClicked]);
+    console.log("UseEffect rendered");
+    fetch("https://api.quotable.io/random")
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, [buttonClicked]);
 
   function changeQuote() {
-        setOpacity(0);
-        setTimeout(() => {
-          setCurrentQuote(' ' + data.content);
-          setOpacity(1);
-          setAuthor('- ' + data.author);
-        }, 1000);
-        setColor(randomColor);
-        setBackgroundColor(randomColor);
-        setButtonClicked((prev) => !prev);
-      }
+    opacity = 0;
+    setTimeout(() => {
+      opacity = 1;
+      setColor(randomColor());
+    }, 500);
+    setButtonClicked((prev) => !prev);
+  }
 
-  return (
-    <div className='wrapper' style={{backgroundColor: backgroundColor, color: color}}>
-      <div id="quote-box">
+  if (data) {
+    return (
+      <div className="wrapper" style={{ backgroundColor: color, color: color }}>
+        <div id="quote-box">
           <figure>
-          <blockquote id="text" style={{opacity: opacity}}><i className="fa-solid fa-quote-left"></i>{currentQuote}</blockquote>
-          <figcaption id="author" style={{opacity: opacity}}>{author}</figcaption>
+            <blockquote id="text" style={{ opacity: opacity }}>
+              <i className="fa-solid fa-quote-left"></i>
+              {' ' + data.content}
+            </blockquote>
+            <figcaption id="author" style={{ opacity: opacity }}>
+              {'- ' + data.author}
+            </figcaption>
           </figure>
-        <div className="buttons-section">
-          <a id="tweet-quote"  href="twitter.com/intent/tweet" target="_blank"><button style={{backgroundColor: backgroundColor}} className="btn"><i className="fa-brands fa-vk fa-2x"></i></button></a>
-          <a id="new-quote" ><button style={{backgroundColor: backgroundColor}} className="btn" onClick={changeQuote}>New Quote</button></a>
+          <div className="buttons-section">
+            <a id="tweet-quote" href="" target="_blank">
+              <button style={{ backgroundColor: color }} className="btn">
+                <i className="fa-brands fa-vk fa-2x"></i>
+              </button>
+            </a>
+            <a id="new-quote">
+              <button
+                style={{ backgroundColor: color }}
+                className="btn"
+                onClick={changeQuote}
+              >
+                New Quote
+              </button>
+            </a>
+          </div>
         </div>
       </div>
-    </div>
-  )
-}
+    );
+  }
+};
 
 //---------------------------
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   /* <React.StrictMode> */
-    <App />
+  <App />
   /* </React.StrictMode> */
 );
